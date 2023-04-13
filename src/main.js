9d485454af96eeb280336272985442ae995a700f
ahajrizaj1 <ah44255@ubt-uni.net>
2023-04-13 04:52:56 +0200

    first change

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import '../styles/modern-normalize.css'
+import '../styles/style.css'
+import '../styles/components/header.css'
+import '../styles/components/hero.css'
+import '../styles/components/tradeoffs.css'
+import '../styles/components/prevention.css'
+import '../styles/components/footer.css'
+import '../styles/components/mobile-nav.css'
+import '../styles/utils.css'
+
+import burger from './utils/burger'
+burger();
